80471e545db7d17afd6c8657352969b57823a195
DESKTOP-OV7AO87\jr <israelspjr@gmail.com>
2020-08-20 19:45:53 -0300

    Update index.php

--- cursos/index.php
+++ cursos/index.php
@@ -92,7 +92,7 @@ session_destroy();
 		   	</div>
 
     	   	<div class="footer1C">
-		    	<center>  <a href="mailto:<?php echo $config[0]['email'];?>"> <img src="images/email.png" class="ficons2"> </a></center> 
+		    	<center> <div class="emailIndex"> <a href="mailto:<?php echo $config[0]['email'];?>"> <img src="images/email.png" class="ficons2"> <span><?php echo $config[0]['email'];?></span></div></a></center> 
 	    	</div>
     </div>
  
