7482c9593a3624e686f671931a5365c4be98c77a
DESKTOP-OV7AO87\jr <israelspjr@gmail.com>
2020-08-20 12:00:57 -0300

    Create index.php

---
+++ testes/index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Sistema Cursos Companhia de Idiomas</title>
+ <meta http-equiv="refresh"
+   content="2; url=http://www.testeseuidioma.com.br/testes">
+<style>
+.blue {
+  color: #d9eef7;
+  border: solid 1px #0076a3;
+  background: #0095cd;
+  background: -webkit-gradient(linear, left top, left bottom, from(#00adee), to(#0078a5));
+}
+.button {
+	  display: inline-block;
+  zoom: 1;
+  vertical-align: baseline;
+  margin: 0 2px;
+  outline: none;
+  cursor: pointer;
+  text-align: center;
+  text-decoration: none;
+  font: 14px/100% Arial, Helvetica, sans-serif;
+  padding: 0.5em 1.5em 0.5em;
+  text-shadow: 0 1px 1px rgba(0,0,0,.3);
+  -webkit-border-radius: .5em;
+  -moz-border-radius: .5em;
+  border-radius: .5em;
+  -webkit-box-shadow: 0 1px 2px rgba(0,0,0,.2);
+  -moz-box-shadow: 0 1px 2px rgba(0,0,0,.2);
+  box-shadow: 0 1px 2px rgba(0,0,0,.2);
+}
+ </style>
+  
+</head>
+<body>
+<p>Esta página foi movida para:
+   <a href="http://www.testeseuidioma.com.br/testes">testes</a></p>
+<!--<center><strong><a href="/cursos/admin/" class="blue button" > ADM</a></strong></center><br />
+<center><strong><a href="/cursos/professor/" class="blue button" > Professor</a></strong></center><br />
+<center><strong><a href="/cursos/aluno/" class="blue button" > Aluno</a></strong></center><br />
+<center><strong><a href="/cursos/RH/" class="blue button" > RH</a></strong></center><br />-->
+</body>
+</html>
