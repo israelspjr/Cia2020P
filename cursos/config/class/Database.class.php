c0a873324f7ec591d12d9a722264873a9dabdcfc
DESKTOP-OV7AO87\jr <israelspjr@gmail.com>
2020-08-20 14:13:44 -0300

    Create Database.class.php

---
+++ cursos/config/class/Database.class.php
@@ -0,0 +1,145 @@
+<?php
+class Database {
+
+	// class attributes
+	var $connect;
+
+	// constructor
+	function __construct() {
+		$this -> connect(DATABASE_DB);        
+	}
+
+	// constructor
+	function __destruct() {
+		//if( $this->connect ) mysql_close($this->connect);
+	}
+
+	// class methods
+	function connect($database = false) {
+	    $Log = new Log();
+		$mysqli = new mysqlí(DATABASE_SERVER, DATABASE_USER, DATABASE_PASS, DATABASE_DB);
+		
+		if ($mysqli->connect_errno) {
+    		echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+			}
+
+		$this -> connect = $mysqli;
+
+		if (!$this -> connect){
+            $mensagemErro = mysql_errno($this -> connect) . ": " . mysql_error($this -> connect);
+      //      $Log->Log("Erro ao conctar db", 1, $mensagemErro, array('usuario'=>$_SESSION['usuario'],$_SESSION['idUsuario']));
+        }
+
+		if ($database) {
+			$this -> selectDb($database);
+            mysql_set_charset('utf8');
+		}
+
+	}
+
+	function selectDb($database) {
+	  //  $Log = new Log();
+		if (!mysql_select_db($database, $this -> connect)){
+		    $mensagemErro = mysql_errno($this -> connect) . ": " . mysql_error($this -> connect);			
+       //     $Log->Log("Erro ao selecionar db", 1, $mensagemErro, array('usuario'=>$_SESSION['usuario'],$_SESSION['idUsuario']));
+        }
+	}
+
+	function fetchArray($result) {
+		if (!$result) {
+			return false;
+		} else {
+			$array = array_map("stripslashes", mysql_fetch_array($result, MYSQL_ASSOC));
+			return $array;
+		}
+	}
+
+	function query($sql, $log = true) {
+
+	//echo $sql;
+	//   $Log = new Log();
+	    
+	      
+		if (!($query = mysql_query($sql))){        
+		  $mensagemErro = $sql;
+		  $acao = "Erro Ao executar acao: ".mysql_errno($this -> connect) . ": " . mysql_error($this -> connect);
+	//	  $Log->Log($acao, 1, $mensagemErro, array('usuario'=>$_SESSION['usuario'], $_SESSION['idUsuario'])); 
+        }
+      
+        /*if (preg_match("/update/i", $sql)) {
+            $acao = "UPDATE";
+        } else if (preg_match("/insert/i", $sql)) {
+            $acao = "INSERT";
+        } else if (preg_match("/delete/i", $sql)) {
+            $acao = "DELETE";
+        }else{
+            $acao="";    
+        }
+        	  
+	    if(($acao == "UPDATE" || $acao == "INSERT" || $acao == "DELETE") AND $mensagemErro=="" AND $log==true){
+          $mensagem = $sql;
+          $Log->Log($acao, 0, $mensagem, array('usuario'=>$_SESSION['usuario'],$_SESSION['idUsuario']));      
+        }*/
+         
+		return $query;
+
+	}
+
+	function mostraErr($sql = "", $soEmail = false) {
+
+		$mensagemErro = "<br />$sql<br />" . mysql_errno($this -> connect) . ": " . mysql_error($this -> connect);
+		$emails =  array(0 => array("email" => EMAIL_ADM, "nome" => "Administrador"));
+		Uteis::enviarEmail("ERRO SISTEMA", $mensagemErro, $emails);
+		
+		
+		if (!$soEmail) {
+			if ( EMPRESA ) {
+				//echo json_encode(array("mensagem" => "Erro: Nao foi possivel completar, comunique o responsavel. Nao repita a acao enquanto o erro nao for corrigido"));
+				echo "Erro: " . $mensagemErro;
+			} else {				
+				echo "Erro: ".$mensagemErro;
+			}
+			exit ;
+		}
+
+	}
+
+	function numRows($result) {
+		if (!$result) {
+			return false;
+		} else {
+			return mysql_num_rows($result);
+		}
+	}
+
+	function executeQuery($sql) {
+		$result = $this -> query($sql);
+		for ($i = 0; $i < $this -> numRows($result); $i++) {
+			$array[$i] = $this -> fetchArray($result);
+		}
+		mysql_free_result($result);
+       	return $array;
+	}
+
+	function gravarBD($texto) {
+
+		$res = mysql_escape_string(trim($texto));
+        //echo $res;
+		//mysql_real_escape_string
+
+		if( is_numeric($res) ) {
+			return $res;
+		}elseif( is_null($res) || $res === '' ) {
+			return "NULL";
+		} else {
+			return "'" . $res . "'";
+		}
+
+	}
+    
+    function logQuery($sql){
+        $query = mysql_query($sql);
+    }
+
+}
+?>
