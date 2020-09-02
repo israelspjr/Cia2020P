140649cbe3c7898f4cd5b0a56852912118151e6c
DESKTOP-OV7AO87\jr <israelspjr@gmail.com>
2020-09-02 11:20:02 -0300

    css

--- cursos/config/include/css.php
+++ cursos/config/include/css.php
@@ -1,15 +1,15 @@
-<link rel="shortcut icon" type="image/x-icon" href="<?php echo CAMINHO_IMG?>_favicon.ico" />
+<!--<link rel="shortcut icon" type="image/x-icon" href="<?php echo CAMINHO_IMG?>_favicon.ico" />
 
-<link rel="stylesheet" href="<?php echo CAMINHO_CFG?>css/zerar.css" type="text/css" />
+<link rel="stylesheet" href="<?php echo CAMINHO_CFG?>css/zerar.css" type="text/css" />-->
 <link rel="stylesheet" href="<?php echo CAMINHO_CFG?>css/estilo.css" type="text/css" />
 <link rel="stylesheet" href="<?php echo CAMINHO_CFG?>css/menu.css" type="text/css" />
 
 <!-- JQuery Ui -->
-<link href="<?php echo CAMINHO_CFG?>css/jquery/jquery-ui.min.css" rel="stylesheet" type="text/css" />
+<link href="<?php echo CAMINHO_CFG?>css/jquery-ui.min.css" rel="stylesheet" type="text/css" />
 
 <!-- data Tables -->
-<link rel="stylesheet" href="<?php echo CAMINHO_CFG?>css/dataTables/table_jui.css" />
+<link rel="stylesheet" href="<?php echo CAMINHO_CFG?>css/table_jui.css" />
 
 <!-- Validação -->
-<link href="<?php echo CAMINHO_CFG?>css/cssValidate/validate.css" type="text/css" media="screen" rel="stylesheet" />
-<link href="<?php echo CAMINHO_CFG?>css/cssValidate/button.css" type="text/css" media="screen" rel="stylesheet" />
+<link href="<?php echo CAMINHO_CFG?>css/validate.css" type="text/css" media="screen" rel="stylesheet" />
+<link href="<?php echo CAMINHO_CFG?>css/button.css" type="text/css" media="screen" rel="stylesheet" />
