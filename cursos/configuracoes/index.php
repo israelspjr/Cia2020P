3531dc2db6a9029ed91231276c29e2627a9629de
DESKTOP-OV7AO87\jr <israelspjr@gmail.com>
2020-09-02 11:09:33 -0300

    Create index.php

---
+++ cursos/configuracoes/index.php
@@ -0,0 +1,59 @@
+<?php  
+require_once($_SERVER['DOCUMENT_ROOT']."/cursos/config/padrao.php");
+$Configuracoes = new Configuracoes();
+
+$ondeAtualiza = "#centro";
+
+if( isset($_REQUEST["tr"]) ){
+  
+  $arrayRetorno = array();
+  
+  $idAcervo = $_REQUEST["idAcervo"];
+  $ordem = $_REQUEST["ordem"];  
+  
+  $arrayRetorno["updateTr"] = $Configuracoes->selectConfigTr();
+  $arrayRetorno["tabela"] = "#tb_lista_config";
+  $arrayRetorno["ordem"] = $ordem;
+  
+  echo json_encode($arrayRetorno);
+  exit;   
+}
+?>
+<fieldset>
+  <legend>Configurações de sistema</legend>
+
+
+<div class="lista">
+  <table id="tb_lista_config" class="registros">
+    <thead>
+      <tr>
+        <th>Nome da empresa</th>
+        <th>Logo</th>
+        <th>WhatsApp</th>
+        <th>Email</th>        
+        <th>Site</th>
+        <th>Rodapé</th>  
+        <th>Cabeçalho</th>                        
+      </tr>
+    </thead>
+    <tbody>
+      <?php            
+        echo $Configuracoes->selectConfigTr($where);
+      ?>
+    </tbody>
+    <tfoot>
+      <tr>       
+         <th>Nome da empresa</th>
+        <th>Logo</th>
+        <th>WhatsApp</th>
+        <th>Email</th>        
+        <th>Site</th>
+        <th>Rodapé</th>  
+        <th>Cabeçalho</th>       </tr>
+    </tfoot>
+  </table>
+</div>
+</fieldset>
+<script>
+tabelaDataTable('tb_lista_acervo');
+</script> 
