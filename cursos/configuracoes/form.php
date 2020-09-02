2a06dcdb9567a6008358335bcb5e1f3e0e510191
DESKTOP-OV7AO87\jr <israelspjr@gmail.com>
2020-09-02 15:08:20 -0300

    Update form.php

--- cursos/configuracoes/form.php
+++ cursos/configuracoes/form.php
@@ -3,8 +3,8 @@ require_once($_SERVER['DOCUMENT_ROOT']."/cursos/config/padrao.php");
 $Configuracoes = new Configuracoes();
  
  $Configuracoes->setIdConfig(1);
-// $valorConfig = $Configuracoes->selectConfig("WHERE idConfig=1"); 
-$seguranca = $Configuracoes->getSeguranca();
+// $valorConfig = $Configuracoes->selectConfig("WHERE idConfig=1"); /
+//$seguranca = $Configuracoes->getSeguranca();
 
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
