7a8b907900b9dfee82ecf76c3c296e15ef98b959
DESKTOP-OV7AO87\jr <israelspjr@gmail.com>
2020-09-08 09:57:00 -0300

    Update acao.php

--- cursos/configuracoes/acao.php
+++ cursos/configuracoes/acao.php
@@ -106,6 +106,55 @@ if($_POST['acao']=="foto"){
     }else{
         $arrayRetorno['mensagem'] = "Selecione uma imagem";
     }
+} elseif($_POST['acao']=="fav"){
+	/* formatos de imagem permitidos */
+    $permitidos = array(".ico");
+    $pasta = CAMINHO_UP_ROOT."imagem/empresa/";
+	
+    if(isset($_POST)){
+        $nome_imagem    = $_FILES['foto']['name'];
+        $tamanho_imagem = $_FILES['foto']['size'];
+         
+        /* pega a extensão do arquivo */
+        $ext = strtolower(strrchr($nome_imagem,"."));
+         
+        /*  verifica se a extensão está entre as extensões permitidas */
+        if(in_array($ext,$permitidos)){
+             
+            /* converte o tamanho para KB */
+            $tamanho = round($tamanho_imagem / 1024);
+             
+            if($tamanho < 1024){ //se imagem for até 1MB envia
+                $nome_atual = md5(uniqid(time())).$ext; //nome que dará a imagem
+                $tmp = $_FILES['foto']['tmp_name']; //caminho temporário da imagem
+                 
+                /* se enviar a foto, insere o nome da foto no banco de dados */
+								if(move_uploaded_file($tmp, $pasta.$nome_atual)){
+								
+									//Redimensiona novamente a imagem, mantendo a original.
+							//		$imgPointer = $img->prepareImage($pasta.$nome_atual);
+							//		$img->prepareResize($imgPointer, 100, 150);
+							//		$img->createThumbNail($imgPointer, $pasta."miniatura-".$nome_atual);
+									
+									$arrayRetorno['elementoAtualizar'][0] = $_POST['destino'];
+									
+									$arrayRetorno['valor2'][0] = "<img src='".CAMINHO_UP."imagem/empresa/".$nome_atual."' width=\"50px\"/>
+									<input type=\"hidden\" name=\"foto_oculta\" value=\"".$nome_atual."\">"; 
+									
+									$arrayRetorno['mensagem'] = "Carregado com sucesso.";
+								
+                }else{
+                    $arrayRetorno['mensagem'] = "Falha ao enviar";					
+                }
+            }else{
+                $arrayRetorno['mensagem'] = "A imagem deve ser de no máximo 2MB";
+            }
+        }else{
+            $arrayRetorno['mensagem'] = "Somente são aceitos arquivos do tipo Imagem";
+        }
+    }else{
+        $arrayRetorno['mensagem'] = "Selecione uma imagem";
+    }
 }
 
 echo json_encode($arrayRetorno);
