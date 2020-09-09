875018e00a2a48a6d96fd0d896e78b0645d13ab7
DESKTOP-OV7AO87\jr <israelspjr@gmail.com>
2020-09-09 11:33:53 -0300

    Update login.php

--- cursos/admin/login.php
+++ cursos/admin/login.php
@@ -1,7 +1,8 @@
 <?php
 $pgLogin = true;
 require_once($_SERVER['DOCUMENT_ROOT']."/cursos/config/admin.php");
-
+echo $_POST['cpf'];
+echo $_POST['password'];
 if($_POST['cpf'] != '' && $_POST['password'] != ''){		
 	if(!$Login->efetuarLogin($_POST['cpf'], EncryptSenha::B64_Encode($_POST['password']))){ 			
 		Uteis::alertJava("Login ou senha inválidos.", true);
