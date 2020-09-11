4ed8f7e68d3820a9f6c379c38451bb0285033429
DESKTOP-OV7AO87\jr <israelspjr@gmail.com>
2020-09-11 10:41:44 -0300

    Create portais.php

---
+++ cursos/config/portais.php
@@ -0,0 +1,22 @@
+<?php
+require_once "padrao.php";
+
+define("CAMINHO_UNICO", "/cursos/portais/");
+define("CAMINHO_MODULO", CAMINHO_UNICO . "modulos/");
+define("CAMINHO_CONFIG", CAMINHO_MODULO . "configuracoes/");
+define("CAMINHO_CAD", CAMINHO_MODULO . "cadastro/");
+define("CAMINHO_GRUPO", CAMINHO_MODULO . "grupo/");
+define("CAMINHO_PSA", CAMINHO_MODULO . "psa/");
+define("CAMINHO_PAG", CAMINHO_MODULO . "demonstrativoPagamento/");
+define("CAMINHO_BH", CAMINHO_MODULO . "bancoHoras/");
+
+require_once $_SERVER['DOCUMENT_ROOT']."/../_config.php";
+
+$Login = new Login();
+
+if ($Login -> verificarLogin_Unico() /*&& $pgUnico*/) {
+   header('Location:/cursos/portais/index.php');
+} elseif (!($Login -> verificarLogin_Unico()) && ($pgUnico == 1)) {
+   echo '<meta http-equiv="refresh" content="0;url=/cursos/">';
+}
+?>
