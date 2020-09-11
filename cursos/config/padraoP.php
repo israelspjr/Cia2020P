8959df677a8b4fe692bf91ba04ef3bdf95696fb4
DESKTOP-OV7AO87\jr <israelspjr@gmail.com>
2020-09-11 11:15:52 -0300

    Update padraoP.php

--- cursos/config/padraoP.php
+++ cursos/config/padraoP.php
@@ -41,9 +41,9 @@ require_once $_SERVER['DOCUMENT_ROOT']."/../_config.php";
 function __autoload($class) {
 	
 	$caminhoClass = $_SERVER['DOCUMENT_ROOT'].CAMINHO_CFG."class/".$class.".class.php";
-		
+	echo $caminhoClass;	
 	if( file_exists($caminhoClass) ){ 
-	echo $caminhoClass;
+	
 		require_once $caminhoClass;
 		return true; 
 	}else{
