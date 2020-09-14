8e188ffaba5c701b2167a2d5e7117df461086028
DESKTOP-OV7AO87\jr <israelspjr@gmail.com>
2020-09-14 15:41:04 -0300

    Update index.php

--- cursos/portais/index.php
+++ cursos/portais/index.php
@@ -1,7 +1,7 @@
 <?php 
 $pgUnico = true;
 require_once($_SERVER['DOCUMENT_ROOT'] . "/cursos/config/portais.php");
-require_once($_SERVER['DOCUMENT_ROOT'] . CAMINHO_CFG . "include/js_area.php");
+require_once($_SERVER['DOCUMENT_ROOT'] . CAMINHO_CFG . "include/js.php");
 require_once($_SERVER['DOCUMENT_ROOT'] . CAMINHO_CFG . "include/css_area.php");
 
 $FolhaFrequencia = new FolhaFrequencia();
