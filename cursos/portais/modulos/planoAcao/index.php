e73816be57f5d16a0d040935286889fe4263d4b3
DESKTOP-OV7AO87\jr <israelspjr@gmail.com>
2020-09-14 17:02:14 -0300

    Create index.php

---
+++ cursos/portais/modulos/planoAcao/index.php
@@ -0,0 +1,17 @@
+<?php  
+require_once($_SERVER['DOCUMENT_ROOT']."/cursos/config/portais.php");
+
+$PlanoAcao = new PlanoAcao();
+
+$idPlanoAcao = $_REQUEST['idPlanoAcao'];
+?>
+
+<!--<div id="dadosDemonstrativo"  class="conteudo_nivel">
+  <div id="fechar_nivel" class="fechar" onclick="fecharNivel();" title="Fechar"></div>
+	<br />--><?php
+    $PlanoAcao->setIdPlanoAcao($idPlanoAcao); 
+    echo $PlanoAcao->ImprimePlanoAcao("3");    
+    ?>
+</div>
+<button class="button gray" onclick="zerarCentro();carregarModulo('modulos/ff/index.php', '#centro')">Fechar</button>
+ 
