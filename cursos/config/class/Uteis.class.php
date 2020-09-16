fe5e8d3c471f2b9b75f092b56c19fb79b9e13166
DESKTOP-OV7AO87\jr <israelspjr@gmail.com>
2020-09-16 20:13:21 -0300

    mailer

--- cursos/config/class/Uteis.class.php
+++ cursos/config/class/Uteis.class.php
@@ -39,9 +39,48 @@ class Uteis {
 	  
 	  $config = new Configuracoes();
 	  
-    require_once 'mailer/class.phpmailer.php';
-    require_once 'mailer/class.smtp.php';
+    require_once 'mailer/phpMailer.php';
 	
+	
+	$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
+    $mail->isSMTP();                                            // Send using SMTP
+    $mail->Host       = 'smtp1.example.com';                    // Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+    $mail->Username   = 'user@example.com';                     // SMTP username
+    $mail->Password   = 'secret';                               // SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+    $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+
+    //Recipients
+    $mail->setFrom('from@example.com', 'Mailer');
+    $mail->addAddress('joe@example.net', 'Joe User');     // Add a recipient
+    $mail->addAddress('ellen@example.com');               // Name is optional
+    $mail->addReplyTo('info@example.com', 'Information');
+    $mail->addCC('cc@example.com');
+    $mail->addBCC('bcc@example.com');
+
+    // Attachments
+    $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+    $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+
+    // Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = 'Here is the subject';
+    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
+	
+ //   require_once 'mailer/class.smtp.php';
+	/*
     $mailer = new PHPMailer();
 	self:pr($mailer);
 	self::pr($config);
@@ -143,7 +182,7 @@ class Uteis {
  //           $mailer->ClearAttachments();
             return true;
         }
-    
+    */
 
   }
 
