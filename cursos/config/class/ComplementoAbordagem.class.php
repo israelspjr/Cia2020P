f121145338c903f9f0d26c5465eaa5158e0709b4
DESKTOP-OV7AO87\jr <israelspjr@gmail.com>
2020-09-16 14:10:53 -0300

    Create ComplementoAbordagem.class.php

---
+++ cursos/config/class/ComplementoAbordagem.class.php
@@ -0,0 +1,188 @@
+<?php
+class ComplementoAbordagem extends Database {
+	// class attributes
+	var $idComplementoAbordagem;
+	var $item;
+	var $inativo;
+	var $padrao;
+	var $nome;
+	var $excluido;
+
+	// constructor
+	function __construct() {
+		parent::__construct();
+		$this -> idComplementoAbordagem = "NULL";
+		$this -> item = "NULL";
+		$this -> inativo = "NULL";
+		$this -> padrao = "NULL";
+		$this -> nome = "NULL";
+		$this -> excluido = "0";
+
+	}
+
+	function __destruct() {
+		parent::__destruct();
+	}
+
+	// class methods
+	function setIdComplementoAbordagem($value) {
+		$this -> idComplementoAbordagem = ($value) ? $this -> gravarBD($value) : "NULL";
+	}
+
+	function setItem($value) {
+		$this -> item = ($value) ? $this -> gravarBD($value) : "NULL";
+	}
+
+	function setInativo($value) {
+		$this -> inativo = ($value) ? $this -> gravarBD($value) : "0";
+	}
+
+	function setPadrao($value) {
+		$this -> padrao = ($value) ? $this -> gravarBD($value) : "NULL";
+	}
+
+	function setNome($value) {
+		$this -> nome = ($value) ? $this -> gravarBD($value) : "NULL";
+	}
+
+	function setExcluido($value) {
+		$this -> excluido = ($value) ? $this -> gravarBD($value) : "0";
+	}
+  function getNome(){
+    return $this->nome;
+  }
+  function getItem(){
+    return $this->item;
+  }
+	/**
+	 * addComplementoAbordagem() Function
+	 */
+	function addComplementoAbordagem() {
+		$sql = "INSERT INTO ComplementoAbordagem (item, inativo, padrao, nome, excluido) VALUES ($this->item, $this->inativo, $this->padrao, $this->nome, $this->excluido)";
+		$result = $this -> query($sql, true);
+		return mysql_insert_id($this -> connect);
+	}
+
+	/**
+	 * deleteComplementoAbordagem() Function
+	 */
+	function deleteComplementoAbordagem() {
+		$sql = "DELETE FROM ComplementoAbordagem WHERE idComplementoAbordagem = $this->idComplementoAbordagem";
+		$result = $this -> query($sql, true);
+	}
+
+	/**
+	 * updateFieldComplementoAbordagem() Function
+	 */
+	function updateFieldComplementoAbordagem($field, $value) {
+		$value = ($value != "NULL") ? $this -> gravarBD($value) : $value;
+		$sql = "UPDATE ComplementoAbordagem SET " . $field . " = " . $value . " WHERE idComplementoAbordagem = $this->idComplementoAbordagem";
+		$result = $this -> query($sql, true);
+	}
+
+	/**
+	 * updateComplementoAbordagem() Function
+	 */
+	function updateComplementoAbordagem() {
+		$sql = "UPDATE ComplementoAbordagem SET item = $this->item, inativo = $this->inativo, padrao = $this->padrao, nome = $this->nome WHERE idComplementoAbordagem = $this->idComplementoAbordagem";
+		$result = $this -> query($sql, true);
+	}
+
+	/**
+	 * selectComplementoAbordagem() Function
+	 */
+	function selectComplementoAbordagem($where = "WHERE 1") {
+		$sql = "SELECT SQL_CACHE idComplementoAbordagem, item, inativo, padrao, nome, excluido FROM ComplementoAbordagem " . $where;
+    //echo $sql;
+		return $this -> executeQuery($sql);
+	}
+
+	/**
+	 * selectComplementoAbordagemTr() Function
+	 */
+	function selectComplementoAbordagemTr($caminhoAbrir, $caminhoAtualizar, $ondeAtualiza, $where = "", $idPai = "", $caminhoModulo = "") {
+		$sql = "SELECT SQL_CACHE idComplementoAbordagem, item, inativo, padrao, nome, excluido FROM ComplementoAbordagem " . $where;
+		$result = $this -> query($sql);
+		if (mysql_num_rows($result) > 0) {
+			$html = "";
+			while ($valor = mysql_fetch_array($result)) {
+				$html .= "<tr>";
+
+				$idComplementoAbordagem = $valor['idComplementoAbordagem'];
+				$item = $valor['item'];
+				$inativo = Uteis::exibirStatus(!$valor['inativo']);
+				$padrao = Uteis::exibirStatus($valor['padrao']);
+				$nome = $valor['nome'];
+
+				$html .= "<td>" . $idComplementoAbordagem . "</td>";
+				$html .= "<td class=\"link\" onclick=\"abrirNivelPagina(this, '" . $caminhoAbrir . "?id=" . $valor['idComplementoAbordagem'] . $idPai . "', '" . $caminhoAtualizar . $idPai . "', '$ondeAtualiza')\" >" . $nome . "</td>";
+				$html .= "<td>" . $inativo . "</td>";
+				$html .= "<td>" . $padrao . "</td>";
+				$html .= "<td onclick=\"deletaRegistro('" . $caminhoModulo . "grava.php', " . $valor['idComplementoAbordagem'] . ", '$caminhoAtualizar', '$ondeAtualiza')\">" . "<center><img src=\"" . CAMINHO_IMG . "excluir.png\"></center>" . "</td>";
+				$html .= "</tr>";
+			}
+		}
+		return $html;
+	}
+
+	/**
+	 * selectComplementoAbordagemSelect() Function
+	 */
+	function selectComplementoAbordagemSelect($classes = "", $idAtual = 0, $and = "") {
+		$sql = "SELECT SQL_CACHE idComplementoAbordagem, item, padrao, nome FROM ComplementoAbordagem WHERE inativo = 0 " . $and . " ORDER BY nome ";
+		$result = $this -> query($sql);
+		$html = "<select id=\"idComplementoAbordagem\" name=\"idComplementoAbordagem\" class=\"" . $classes . "\" >";
+		$html .= "<option value=\"\">Selecione</option>";
+
+		while ($valor = mysql_fetch_array($result)) {
+			$selecionado = $idAtual == $valor['idComplementoAbordagem'] ? "selected=\"selected\"" : "";
+			$padrao = $valor['padrao'] ? " (padrão)" : "";
+			$html .= "<option " . $selecionado . " value=\"" . $valor['idComplementoAbordagem'] . "\">" . $valor['nome'] . $padrao . "</option>";
+		}
+		$html .= "</select>";
+		return $html;
+	}
+  
+  function selectAbordagemCheckbox($idPlanoAcao, $idIdioma, $addQuery = "") {
+
+    $sql = "SELECT SQL_CACHE idComplementoAbordagem, item, padrao, nome FROM ComplementoAbordagem WHERE excluido = 0 AND inativo = 0 " . $addQuery . " ORDER BY nome";
+	//echo $sql;
+    $result = $this -> query($sql);
+    
+    $idioma = new Idioma();
+
+    $PlanoAcaoComplemento = new PlanoAcaoComplemento();
+
+    if (mysql_num_rows($result) > 0) {
+      $html = "";
+
+      //CONSULTA SE HÁ ALGUM complemento cadastrado
+      $where = " WHERE planoAcao_idPlanoAcao = " . $idPlanoAcao;
+      $Acompanhamento = $PlanoAcaoComplemento -> selectPlanoAcaoComplemento($where);
+
+      while ($valor = mysql_fetch_array($result)) {
+
+        if ($Acompanhamento) {
+          $where = " WHERE planoAcao_idPlanoAcao = " . $idPlanoAcao . " AND complementoAbordagem_idComplementoAbordagem = " . $valor['idComplementoAbordagem'];
+          $checked = $PlanoAcaoComplemento -> selectPlanoAcaoComplemento($where) ? " checked " : "";
+        } 
+
+        $html .= "<div  >";
+
+        $html .= "<img id=\"img_abordagem_" . $valor['idComplementoAbordagem'] . "\" src=\"" . CAMINHO_IMG . ($checked == "" ? "mais" : "menos") . ".png\" onclick=\"abrirFormulario('div_abordagem_" . $valor['idComplementoAbordagem'] . "', 'img_abordagem_" . $valor['idComplementoAbordagem'] . "')\" style=\"float:left; margin-right:10px;\" />";
+
+        $html .= "<label for=\"check_abordagem_" . $valor['idComplementoAbordagem'] . "\">";
+
+        $html .= "<input type=\"checkbox\" id=\"check_abordagem_" . $valor['idComplementoAbordagem'] . "\" name=\"check_abordagem_" . $valor['idComplementoAbordagem'] . "\" $checked value=\"1\" />";
+
+        $html .= "<strong>" . strtoupper($valor['nome']) . "</strong> ";
+
+        $html .= "</label>";
+
+        $html .= "<div id=\"div_abordagem_" . $valor['idComplementoAbordagem'] . "\" style=\"display:" . ($checked == "" ? "none" : "block") . "; padding:1em; text-align:justify;\" >" . $valor['item'] . "</div></div><br/><br/>";
+      }
+    }
+    return $html;
+  }
+
+}
