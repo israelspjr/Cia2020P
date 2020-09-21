8e01c1089a37ab5d924abdd7a91690131afd8f0c
DESKTOP-OV7AO87\jr <israelspjr@gmail.com>
2020-09-21 18:38:37 -0300

    Update provaR.php

--- cursos/portais/modulos/provas/provaR.php
+++ cursos/portais/modulos/provas/provaR.php
@@ -8,7 +8,7 @@ require_once "filtrosR.php";
 ?>
 
 <div class="linha-inteira">
-	<button class="button gray" onclick="postForm('form_rel', '<?php echo CAMINHO_RELAT."prova/provaRAcao.php"?>')"> Exportar relatório</button>
+	<button class="button gray" onclick="postForm('form_rel', '<?php echo "modulos/prova/provaRAcao.php"?>')"> Exportar relatório</button>
 </div>
 
 <?php
