f0efd20cac9f7374201bae552390cbf5e2f6ed8e
DESKTOP-OV7AO87\jr <israelspjr@gmail.com>
2020-09-21 18:40:55 -0300

    Create provasRAcao.php

---
+++ cursos/portais/modulos/provas/provasRAcao.php
@@ -0,0 +1,24 @@
+<?php
+require_once($_SERVER['DOCUMENT_ROOT']."/cursos/config/portais.php");
+
+$CalendarioProva = new CalendarioProva();
+$arrayRetorno = array();
+
+require_once "filtrosR.php";
+
+$status = $_POST['status'];
+
+if ($status == 1) {
+	$arrayRetorno['excel'] = $Relatorio->relatorioProvaAplicadas($where, $tipo, $excel, $status, $where2, "", "", $campos, $camposNome);
+} elseif ($status == 2) {
+	$arrayRetorno['excel'] = $Relatorio->relatorioProvaAgendadas($where, $tipo, $excel, $status, $where2, 1, $campos, $camposNome, 2);
+} else {
+	$arrayRetorno['excel'] = $Relatorio->relatorioProvaAplicadas($where, $tipo, $excel, $status, $where2, "", "", $campos, $camposNome);
+	
+	$arrayRetorno['excel'] = $Relatorio->relatorioProvaAgendadas($where, $tipo, $excel, $status, $where2, 1, $campos, $camposNome, 2);
+}
+	
+
+echo json_encode($arrayRetorno);
+
+?>
