25daa9a4466d863617bd36016181dc71cb27801a
DESKTOP-OV7AO87\jr <israelspjr@gmail.com>
2020-10-05 09:44:03 -0300

    Update notas.php

--- cursos/portais/modulos/provas/notas.php
+++ cursos/portais/modulos/provas/notas.php
@@ -101,7 +101,8 @@ $provaOral = $valorNivel[0]['provaOral'];
 													WHERE calendarioProva_idCalendarioProva=".$idCalendarioProva." 
 													AND integranteGrupo_idIntegranteGrupo=".$idIntegranteGrupo." 
 													AND itenProva_idItenProva=".$idItenProva;								
-													$rsNota = mysqli_fetch_array($Prova->query($sql));
+												    $rs = $Prova->query($sql);
+													$rsNota = mysqli_fetch_array($rs);
 													
 													$aulaFixaIds = $AulaDataFixa->selectAulaDataFixa(" AND planoAcaoGrupo_idPlanoAcaoGrupo = ".$idPlanoAcaoGrupo);
 													$aulaPermanenteIds = $AulaPermanenteGrupo->selectAulaPermanenteGrupo(" WHERE planoAcaoGrupo_idPlanoAcaoGrupo = ".$idPlanoAcaoGrupo);
