c6d548159e4da28992cc2b0eb21eab5f92e009fe
DESKTOP-OV7AO87\jr <israelspjr@gmail.com>
2020-10-05 14:47:51 -0300

    novapsa

---
+++ cursos/portais/modulos/novaPsa/filtros.php
@@ -0,0 +1,52 @@
+<?php
+require_once($_SERVER['DOCUMENT_ROOT']."/cursos/config/portais.php");
+
+//MONTA CAMPO
+$campos = $_POST['sel_lista_padrao'];
+$camposNome = $_POST['sel_lista_padraoNome'];
+array_unshift($campos,"Grupos");
+array_unshift($camposNome,"Grupos");
+
+
+ $rs = count($_POST['idGrupo']);
+ $idGrupos = implode(",", $_POST['idGrupo']);
+
+ if (($idGrupos != "") && (count($idGrupos) > 0))  {
+        $gerente .= " AND G.idGrupo in (".$idGrupos.")";  
+ }
+//}
+
+$dataReferencia1 = $_POST['dataReferencia'];
+$dataReferencia1a = strtotime(str_replace('/', '-', $dataReferencia1)); 
+
+$dataIni = "01/03/2015";
+$dataIni = strtotime(str_replace('/', '-', $dataIni)); 
+
+if($dataReferencia1a == "" || ($dataReferencia1a < $dataIni))
+    $dataReferencia = "01/03/2015";
+else    
+    $dataReferencia = $dataReferencia1;
+	
+	
+$dataReferencia2 = $_POST['dataReferencia2'];	
+
+
+$dataReferencia2a = strtotime(str_replace('/', '-', $dataReferencia2)); 
+
+    
+if($dataReferencia2a == "" || ($dataReferencia2a < $dataIni))   { 
+    $dataReferencia2 = date("d/m/Y");
+}
+
+//if($dataReferencia1 && $dataReferencia2) 
+	$where .= " AND DATE(PIG.dataReferencia) BETWEEN '2018-09-01' AND '".$dataReferencia2."' ";
+	
+if (($_POST['mostrarComentarios'] == "") || (!isset($_POST['mostrarComentarios']))){
+	$mostrarComentarios = 0;
+} else {
+	$mostrarComentarios = 1;
+}
+
+	$idProfessor = $_SESSION['idProfessor_SS']; 
+?>
+
---
+++ cursos/portais/modulos/novaPsa/index.php
@@ -0,0 +1,116 @@
+<?php  
+require_once($_SERVER['DOCUMENT_ROOT']."/cursos/config/portaisr.php");
+
+$TipoEnderecoVirtual = new TipoEnderecoVirtual();
+$PsaProfessor = new PsaProfessor();
+$PsaRegular = new PsaRegular();
+$Gerente = new Gerente();
+$ClientePj = new ClientePj();
+$Grupo = new Grupo();
+
+//$nomeCliente = $ClientePj->getNome($_SESSION['idClientePj_SS']);
+
+$arrItens = array();
+
+//PADRÃO
+$arrItens_padrao[] = array(0 => "aluno", 1 => "Aluno");
+$arrItens_padrao[] = array(0 => "dataReferencia", 1 => "Data da pesquisa");
+$arrItens_padrao[] = array(0 => "nomeProfessor", 1 => "Nome Professor");
+
+$rsPsaProfessor = $PsaProfessor->selectPsaProfessor(" WHERE excluido = 0 AND inativo = 0 ");
+foreach($rsPsaProfessor as $valor) $arrItens_padrao[] = array(0 => $valor['titulo'], 1 => $valor['titulo']);
+
+$rsPsaRegular = $PsaRegular->selectPsaRegular(" WHERE excluido = 0 AND inativo = 0 ");
+foreach($rsPsaRegular as $valor) $arrItens_padrao[] = array(0 => $valor['titulo'], 1 => $valor['titulo']);
+
+?>
+
+<fieldset>
+  <legend>Relatório de psa</legend>
+  <img src="<?php echo CAMINHO_IMG."menos.png"?>" title="Abrir/Fechar formuário" id="img_form_Grupos" 
+onclick="abrirFormulario('div_form_Grupos', 'img_form_Grupos');" />
+  <div class="agrupa" id="div_form_Grupos">
+    <form id="form_rel_pf" class="validate" method="post" action="" onsubmit="return false" >
+    
+      <!--<p><strong>Campos</strong></p>-->
+      <p><strong>Filtros</strong></p>
+      <div class="esquerda">
+     <!--   <label>Selecionados:</label>-->
+        <img src="<?php echo CAMINHO_IMG."menos.png"?>" name="delIten" id="delIten" title="Remover iten" onclick="addIten('#sel_lista_padrao', '#sel_lista_opcional')"/>
+        
+        <?php $sql = " SELECT DISTINCT(G.idGrupo), G.nome AS grupo, PAG.idPlanoAcaoGrupo FROM professor AS P 
+				INNER JOIN aulaGrupoProfessor AS AGP ON AGP.professor_idProfessor = P.idProfessor
+				LEFT JOIN aulaPermanenteGrupo AS AP ON AP.idAulaPermanenteGrupo = AGP.aulaPermanenteGrupo_idAulaPermanenteGrupo
+				LEFT JOIN aulaDataFixa AS AF ON AF.idAulaDataFixa = AGP.aulaDataFixa_idAulaDataFixa
+				INNER JOIN planoAcaoGrupo AS PAG ON PAG.inativo = 0 AND 
+					(PAG.idPlanoAcaoGrupo = AP.planoAcaoGrupo_idPlanoAcaoGrupo OR PAG.idPlanoAcaoGrupo = AF.planoAcaoGrupo_idPlanoAcaoGrupo)
+				INNER JOIN grupo AS G ON G.idGrupo = PAG.grupo_idGrupo 
+				WHERE ( AGP.dataFim >= CURDATE() OR AGP.dataFim IS NULL OR AGP.dataFim = '') AND P.idProfessor = " . $_SESSION['idProfessor_SS'];
+				
+				$rsGrupos = Uteis::executarQuery($sql);
+
+				$idsGrupos = array();
+				
+				for ($x=0;$x<count($rsGrupos);$x++) {
+			
+			$idGrupos .= $rsGrupos[$x]['idGrupo'].",";
+				 
+			 }
+			 $idGrupos .= '-1';
+			 
+				?>
+				 <p>
+            <label>Grupos:</label>
+                   <?php 
+			 echo $Grupo->selectGrupoSelectMult("",""," WHERE G.idGrupo in (".$idGrupos.") ");
+			 ?>    
+        
+ 
+        </p>
+	
+          	<?php foreach($arrItens_padrao as $iten){?>
+          	    <input type="hidden" name="sel_lista_padrao[]" id="sel_lista_padrao" value="<?php echo $iten[0]?>" /> 
+          	    <input type="hidden" name="sel_lista_padraoNome[]" id="sel_lista_padraoNome" value="<?php echo $iten[1]?>" />  
+            
+            <?php }?>                 	
+     
+          <p>
+            <label>Data da pesquisa:</label>
+            de
+            <input type="text" name="dataReferencia" id="dataReferencia" class="data" value="01/09/2018" disabled="disabled"/>
+            a
+            <input type="date" name="dataReferencia2" id="dataReferencia2" class="data" value=""  />
+          </p>
+          <p>
+          <Label>Mostrar comentários? </Label>
+          <input type="checkbox" value="1" id="mostrarComentarios" name="mostrarComentarios" />
+          </p>
+        </div>
+        <div class="direita">
+         <p>
+          <Label>Mostrar PSA Pendentes </Label>
+          <input type="checkbox" value="1" id="psaPendentes" name="psaPendentes" />
+          </p> </div>
+      </div>
+      
+      <div class="linha-inteira" >
+        <button class="Bblue" onclick="geraRel()">
+        Gerar relatório</button>
+      </div>
+    </form>
+  </div>
+  
+</fieldset>
+<fieldset>
+  <legend>Resultado da pesquisa</legend>
+  <div id="res_rel" class="lista" ></div>
+</fieldset>
+<script> 
+function geraRel(){
+addItenPersonalizado('#sel_lista_padrao', '#sel_lista_padraoNome');
+	selecionaTudoSelect('sel_lista_padrao', 'sel_lista_padraoNome');
+    postForm_relatorio('img_form_Grupos', '', 'form_rel_pf', '<?php echo "modulos/novaPsa/psa.php"?>', 'res_rel');
+}
+
+//ativarForm();	
+</script> 
