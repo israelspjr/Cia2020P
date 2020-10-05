026f1f9abccb32b0166db5127ecc16934e119bb9
DESKTOP-OV7AO87\jr <israelspjr@gmail.com>
2020-10-05 14:06:32 -0300

    Update diaAulaFFIndividualAcao.php

--- cursos/portais/modulos/ff/professor/diaAulaFFIndividualAcao.php
+++ cursos/portais/modulos/ff/professor/diaAulaFFIndividualAcao.php
@@ -1,5 +1,5 @@
 <?php
-require_once($_SERVER['DOCUMENT_ROOT']."/cursos/config/professor.php");
+require_once($_SERVER['DOCUMENT_ROOT']."/cursos/config/portais.php");
 
 $DiaAulaFF = new DiaAulaFF();
 $DiaAulaFFIndividual = new DiaAulaFFIndividual();
