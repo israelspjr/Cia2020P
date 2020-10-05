b834e748e161ce82c7e1e87baca80e84566cddeb
DESKTOP-OV7AO87\jr <israelspjr@gmail.com>
2020-10-05 12:22:25 -0300

    Update js.php

--- cursos/config/include/js.php
+++ cursos/config/include/js.php
@@ -4,43 +4,43 @@
 <!-- Funções form -->
 <script src="<?php echo CAMINHO_CFG?>js/form2.js" language="javascript" type="text/javascript"></script>
 
-<script src="<?php echo CAMINHO_CFG?>js/login.js" language="javascript" type="text/javascript"></script>
+<!--<script src="<?php echo CAMINHO_CFG?>js/login.js" language="javascript" type="text/javascript"></script>
 
 
 <!-- JQuery Ui -->
 <!--<script src="<?php echo CAMINHO_CFG?>js/jquery-ui.min.js" language="javascript" type="text/javascript"></script>
 
 <!-- Editor -->
-<script src="<?php echo CAMINHO_CFG?>js/tinymce.min.js" language="javascript" type="text/javascript" ></script>
+<!--<script src="<?php echo CAMINHO_CFG?>js/tinymce.min.js" language="javascript" type="text/javascript" ></script>
 
 <!-- data Tables 
 <script src="<?php echo CAMINHO_CFG?>js/jquery.dataTables.min.js" language="javascript" type="text/javascript" ></script>-->
 <!--<script src="<?php echo CAMINHO_CFG?>js/dataTables.fixedHeader.min.js" language="javascript" type="text/javascript" ></script>-->
-<script src="<?php echo CAMINHO_CFG?>DataTables/datatables.js" language="javascript" type="text/javascript" ></script>
+<!--<script src="<?php echo CAMINHO_CFG?>DataTables/datatables.js" language="javascript" type="text/javascript" ></script>
 
 <!-- Atualização para sortear data -->
-<script src="//cdnjs.cloudflare.com/ajax/libs/moment.js/2.8.4/moment.min.js"></script>
+<!--<script src="//cdnjs.cloudflare.com/ajax/libs/moment.js/2.8.4/moment.min.js"></script>
 <script src="//cdn.datatables.net/plug-ins/1.10.7/sorting/datetime-moment.js"></script>
 
 <!-- Funções uteis -->
-<script src="<?php echo CAMINHO_CFG?>js/uteis.js" language="javascript" type="text/javascript"></script>
+<!--<script src="<?php echo CAMINHO_CFG?>js/uteis.js" language="javascript" type="text/javascript"></script>
 
 <!-- Eventos gerais-->
-<script src="<?php echo CAMINHO_CFG?>js/eventos.js" language="javascript" type="text/javascript"></script>
+<!--<script src="<?php echo CAMINHO_CFG?>js/eventos.js" language="javascript" type="text/javascript"></script>
 
 <!-- Mask-->
-<script src="<?php echo CAMINHO_CFG?>/js/jquery.mask.min.js" type="text/javascript"></script>
+<!--<script src="<?php echo CAMINHO_CFG?>/js/jquery.mask.min.js" type="text/javascript"></script>
 
 <!-- Validação -->
-<script src="<?php echo CAMINHO_CFG?>js/jquery.validate.js" language="javascript" type="text/javascript"></script>
+<!--<script src="<?php echo CAMINHO_CFG?>js/jquery.validate.js" language="javascript" type="text/javascript"></script>
 
 <!-- PRINT ELEMENT-->
-<script src="<?php echo CAMINHO_CFG?>js/jquery.printArea.js" language="javascript" type="text/javascript"></script>
+<!--<script src="<?php echo CAMINHO_CFG?>js/jquery.printArea.js" language="javascript" type="text/javascript"></script>
 <!-- Carregar Google APi-->
-<script type="text/javascript" src="<?php echo CAMINHO_CFG?>js/jsapi.js"></script>
+<--<script type="text/javascript" src="<?php echo CAMINHO_CFG?>js/jsapi.js"></script>
 
 <script type="text/javascript">
-	$(document).ready(function(){		
+/*	$(document).ready(function(){		
 		eventAbas();			
 		eventFechar();			
 		eventRolarParaTopo();
@@ -48,4 +48,4 @@
 		eventFocoForm();
 		eventMostrarTitle();
 	});//.tooltip();
-</script>
+*/</script>
