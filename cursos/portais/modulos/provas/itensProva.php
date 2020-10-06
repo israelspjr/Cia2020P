54de7a57a72db15a756b66ef240012af588b4592
DESKTOP-OV7AO87\jr <israelspjr@gmail.com>
2020-10-06 11:59:40 -0300

    Update itensProva.php

--- cursos/portais/modulos/provas/itensProva.php
+++ cursos/portais/modulos/provas/itensProva.php
@@ -4,8 +4,7 @@ require_once($_SERVER['DOCUMENT_ROOT']."/cursos/config/portais.php");
 $Prova = new Prova();
 
 $rsItens = $Prova->query("SELECT SQL_CACHE nome FROM itenProva WHERE inativo = 0 AND prova_idProva = ".$_GET['idProva']);
-Uteis::pr($rsItens);
 
-while($valor = mysql_fetch_array($rsItens)) echo "<p>".$valor['nome']."</p>";
+while($valor = mysqli_fetch_array($rsItens)) echo "<p>".$valor['nome']."</p>";
 
 ?>
