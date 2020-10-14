d749b8008ef512ccbf6e7001a48cc1fb984918d2
DESKTOP-OV7AO87\jr <israelspjr@gmail.com>
2020-10-14 10:31:56 -0300

    Update index.php

--- cursos/portais/index.php
+++ cursos/portais/index.php
@@ -177,6 +177,7 @@ onclick="fecharMenu(0);abrirFormulario('menu_area', 'img_form_Menu');" /></li>
 //	carregarModulo('/cursos/mobile/professor/modulos/cadastro/form/professor.php', '#centro');	 
 		 
 <?php 	 } else { ?>
+    $("#centro").html("Por favor aguarde carregar os gráficos!!!");
     carregarModulo('/cursos/portais/aviso.php', '#centro');
 	
 	<?php } ?>
