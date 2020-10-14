a066cb01411b2758c3030c8d05bb2efde84c6c7f
DESKTOP-OV7AO87\jr <israelspjr@gmail.com>
2020-10-14 10:09:27 -0300

    Update aviso.php

--- cursos/portais/aviso.php
+++ cursos/portais/aviso.php
@@ -228,11 +228,12 @@ function psa2(x) {
     type:"POST",
     datatype: "html",
     contentType: "application/x-www-form-urlencoded; charset=utf-8",
-    data:{status:status,idProfessor:idProfessor,gerente:gerente, quantidade:quantidade, x:x}   
-  });
-  retorno.beforeSend: function(){
+	beforeSend:function(){
            $("#centro").dialog('open').html("<p>Por favor espere...</p>");
         },
+    data:{status:status,idProfessor:idProfessor,gerente:gerente, quantidade:quantidade, x:x}   
+  });
+  
   retorno.done(function( html ) {
 	  
 	//  console.log(html);
