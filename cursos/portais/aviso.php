18aa2f56aa9278b13280aa3999f0f8f08f461493
DESKTOP-OV7AO87\jr <israelspjr@gmail.com>
2020-10-14 13:44:05 -0300

    Update aviso.php

--- cursos/portais/aviso.php
+++ cursos/portais/aviso.php
@@ -329,7 +329,7 @@ function psa2(x) {
                 labels: {
 					filter: function(legendItem, data) {
 						console.log(legendItem.index);
-						console.log(data[legendItem.index]);
+						console.log(data.datasets);
 						console.log("ok");
                 			if (legendItem.dataIndex === 0) {
                   				return false;
