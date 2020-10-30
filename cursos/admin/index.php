f52b5d57de61b09823317000458b24666967b99a
DESKTOP-OV7AO87\jr <israelspjr@gmail.com>
2020-10-30 16:58:07 -0300

    Update index.php

--- cursos/admin/index.php
+++ cursos/admin/index.php
@@ -1,3 +1,4 @@
+<!DOCTYPE html>
 <?php
 require_once ($_SERVER['DOCUMENT_ROOT'] . "/cursos/config/admin.php");
 require_once ($_SERVER['DOCUMENT_ROOT'] . CAMINHO_CFG . "include/css.php");
@@ -12,7 +13,7 @@ if($_SESSION['idFuncionario_SS']==""){
 }
 ?>
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!--<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">-->
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
