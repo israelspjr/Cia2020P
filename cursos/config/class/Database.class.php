21b6b4b092d81e3f4efef394d05cfff03719e610
DESKTOP-OV7AO87\jr <israelspjr@gmail.com>
2020-11-09 21:40:20 -0300

    Update Database.class.php

--- cursos/config/class/Database.class.php
+++ cursos/config/class/Database.class.php
@@ -7,7 +7,6 @@ class Database {
 	// constructor
 	function __construct() {
 		$this -> connect();        
-		
 	}
 
 	// constructor
@@ -28,28 +27,19 @@ class Database {
 		if (!$result) {
 			return false;
 		} else {
-		//	$array = array_map("stripslashes", mysqli_fetch_array($result, MYSQL_ASSOC));
-		$array = mysqli_fetch_array($result, MYSQLI_ASSOC);
-//		var_dump($array);
+     		$array = mysqli_fetch_array($result, MYSQLI_ASSOC);
 			return $array;
 		}
 	}
 
 	function query($sql, $log = true) {
-		
-	 //    $link = mysqli_connect(DATABASE_SERVER, DATABASE_USER, DATABASE_PASS, DATABASE_DB);	
-
-	//echo $sql;
-	//   $Log = new Log();
 	    mysqli_set_charset( $this->connect, 'utf8');
 	      
 		if (!($query = mysqli_query($this->connect, $sql))){        
 		  $mensagemErro = $sql;
 		  $acao = "Erro Ao executar acao: ".mysqli_errno($this->connect) . ": " . mysqli_error($this->connect);
-		  
-//		  echo $acao;
-	//	  $Log->Log($acao, 1, $mensagemErro, array('usuario'=>$_SESSION['usuario'], $_SESSION['idUsuario'])); 
-        }
+	
+	        }
      
 		return $query;
 
@@ -63,7 +53,6 @@ class Database {
 				
 		if (!$soEmail) {
 			if ( EMPRESA ) {
-				//echo json_encode(array("mensagem" => "Erro: Nao foi possivel completar, comunique o responsavel. Nao repita a acao enquanto o erro nao for corrigido"));
 				echo "Erro: " . $mensagemErro;
 			} else {				
 				echo "Erro: ".$mensagemErro;
@@ -84,19 +73,12 @@ class Database {
 	function executeQuery($sql) {
 		$result = $this -> query($sql);
 		$array = "";
-	//	if (mysqli_num_rows > 0) {
 		for ($i=0;$i<mysqli_num_rows($result);$i++) {
 			foreach($result as $key => $row)
 				{
 					$array[$key] = $row;
 				}
 			}
-	//	}
-
-/*		for ($i = 0; $i < $this -> count($result); $i++) {
-			echo $i;
-			$array[$i] = $this -> fetchArray($result);
-		}*/
 		mysqli_free_result($result);
        	return $array;
 	}
@@ -104,9 +86,7 @@ class Database {
 	function gravarBD($texto) {
 
 		$res =  trim($texto);
-   //     echo $res;
-		//mysql_real_escape_string
-
+ 
 		if( is_numeric($res) ) {
 			return $res;
 		}elseif( is_null($res) || $res === '' ) {
