7bc860fe906e506f4b1de49ee75a3a64dbd81ab7
DESKTOP-OV7AO87\jr <israelspjr@gmail.com>
2020-11-15 16:50:26 -0300

    Update opcaoAtividadeExtraProfessorClientePf.php

--- cursos/admin/modulos/cadastro/clientePf/include/form/opcaoAtividadeExtraProfessorClientePf.php
+++ cursos/admin/modulos/cadastro/clientePf/include/form/opcaoAtividadeExtraProfessorClientePf.php
@@ -1,6 +1,6 @@
 <?php
 	require_once($_SERVER['DOCUMENT_ROOT']."/cursos/config/admin.php");  
-	
+	error_reporting(E_ALL);
 	$TipoAtividadeExtraProfessor = new TipoAtividadeExtraProfessor();
 	
 	$idClientePf = $_GET['id'];
