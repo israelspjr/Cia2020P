67717243343061149f30589c6b95c1dbad690eee
DESKTOP-OV7AO87\jr <israelspjr@gmail.com>
2020-11-20 09:44:33 -0300

    Update ComplementoAbordagem.class.php

--- cursos/config/class/ComplementoAbordagem.class.php
+++ cursos/config/class/ComplementoAbordagem.class.php
@@ -109,8 +109,8 @@ class ComplementoAbordagem extends Database {
 	 * selectComplementoAbordagemTr() Function
 	 */
 	function selectComplementoAbordagemTr($caminhoAbrir, $caminhoAtualizar, $ondeAtualiza, $where = "", $idPai = "", $caminhoModulo = "") {
-		$sql = "SELECT SQL_CACHE idComplementoAbordagem, item, inativo, padrao, nome, excluido, portalProfessor FROM complementoAbordagem " . $where;
-		echo $sql;
+		$sql = "SELECT SQL_CACHE idComplementoAbordagem, item, inativo, padrao, nome, excluido, portalProfessor FROM ComplementoAbordagem " . $where;
+	//	echo $sql;
 		$result = $this -> query($sql);
 		if (mysqli_num_rows($result) > 0) {
 			$html = "";
