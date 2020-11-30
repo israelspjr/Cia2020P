790b927c1cfc4bf4d806e71670dffc136df30538
DESKTOP-OV7AO87\jr <israelspjr@gmail.com>
2020-11-30 14:15:06 -0300

    Update recuperaSenha.php

--- cursos/admin/recuperaSenha.php
+++ cursos/admin/recuperaSenha.php
@@ -4,29 +4,25 @@ require_once($_SERVER['DOCUMENT_ROOT']."/cursos/config/portais.php");
 
 $msg = 0;
 if(isset($_POST)){
-    $doc = $_POST['documentoUnico'];
-    $nasc = $_POST['nasc'];
-    $senha = $_POST['password'];
-	echo $doc;
-	echo $nasc;
-	echo $senha;
-	
+    $doc = mysql_real_escape_string($_POST['documentoUnico']);
+    $nasc = mysql_real_escape_string($_POST['nasc']);
+    $senha = mysql_real_escape_string($_POST['password']);
+
     if (isset($doc) AND isset($nasc) AND isset($senha)){
         $data = Uteis::gravarData($nasc);
         $senhaAcesso = EncryptSenha::B64_Encode($senha);
 
-        $ClientePf = new ClientePf();
-        $rs = $ClientePf->selectClientepf(" WHERE documentoUnico = '".$doc."' AND dataNascimento='".$data."'");
-		var_dump($rs);
+        $Funcionario = new Funcinario();
+        $rs = $Funcionario->selectFuncionario(" WHERE documentoUnico = '".$doc."' AND dataNascimento='".$data."'");
         if($rs){
-            $idClientePf = $rs[0]["idClientePf"];
+            $idFuncionario = $rs[0]["idFuncionario"];
             $nome = $rs[0]["nome"];
             $ClientePf->setIdClientePf($idClientePf);
             $ClientePf->updateFieldClientepf("SenhaAcesso", $senhaAcesso);
             //$emails = $ClientePf->getEmail($idClientePf, 1);
             $msg = 1;
         }
-     //   var_dump($rs);
+        var_dump($rs);
     }
 }
 $reditectUrl = ($msg==1)? 'login.php?msg=1' : 'recuperaSenhaForm.php?msg=1';
