3078682a7d5231dc19074b0bec60cb52285b8c94
DESKTOP-OV7AO87\jr <israelspjr@gmail.com>
2020-11-30 14:06:39 -0300

    Update recuperaSenhaForm.php

--- cursos/admin/recuperaSenhaForm.php
+++ cursos/admin/recuperaSenhaForm.php
@@ -1,5 +1,5 @@
 <?php
-require_once ($_SERVER['DOCUMENT_ROOT'] . "/cursos/config/admin.php");
+require_once ($_SERVER['DOCUMENT_ROOT'] . "/cursos/config/portais.php");
 require_once ($_SERVER['DOCUMENT_ROOT'] . CAMINHO_CFG . "include/js.php");
 require_once($_SERVER['DOCUMENT_ROOT'] . CAMINHO_CFG . "include/css_area.php");
 
