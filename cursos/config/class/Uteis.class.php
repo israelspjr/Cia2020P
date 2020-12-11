bb12c544decb41dc3a2c2748309f1cd1d39c2807
DESKTOP-OV7AO87\jr <israelspjr@gmail.com>
2020-12-11 12:17:48 -0300

    Update Uteis.class.php

--- cursos/config/class/Uteis.class.php
+++ cursos/config/class/Uteis.class.php
@@ -595,6 +595,31 @@ if ($config[0]['seguranca'] == '') {
       return $semanaAtual;
     }
   }
+  
+  static function getWeeks($date, $rollover)
+    {
+        $cut = substr($date, 0, 8);
+        $daylen = 86400;
+
+        $timestamp = strtotime($date);
+        $first = strtotime($cut . "00");
+        $elapsed = ($timestamp - $first) / $daylen;
+
+        $weeks = 1;
+
+        for ($i = 1; $i <= $elapsed; $i++)
+        {
+            $dayfind = $cut . (strlen($i) < 2 ? '0' . $i : $i);
+            $daytimestamp = strtotime($dayfind);
+
+            $day = strtolower(date("l", $daytimestamp));
+
+            if($day == strtolower($rollover))  $weeks ++;
+        }
+
+        return $weeks;
+    }
+
 
   static function pr($arr, $exit = 0) {
     echo "<pre>";
