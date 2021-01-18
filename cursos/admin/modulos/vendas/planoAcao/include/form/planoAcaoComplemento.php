6c92852a7af6ff6ff6bdafd1cf0ec2c0655408a8
DESKTOP-OV7AO87\jr <israelspjr@gmail.com>
2021-01-18 17:13:24 -0300

    Update planoAcaoComplemento.php

--- cursos/admin/modulos/vendas/planoAcao/include/form/planoAcaoComplemento.php
+++ cursos/admin/modulos/vendas/planoAcao/include/form/planoAcaoComplemento.php
@@ -6,12 +6,12 @@
 ?>
 <fieldset>
   <legend>Complemento de Abordagem</legend>
- <form id="form_Abordagem"  class="validate" action="" method="post"  enctype="multipart/form-data" onsubmit="return false" >
+ <form id="form_Abordagem2"  class="validate" action="" method="post"  enctype="multipart/form-data" onsubmit="return false" >
      <?php echo $Complemento->selectAbordagemCheckbox($idPlanoAcao, "", "", "", "d"); ?> 
     <br />
     <div class="linha-inteira">
       <p>
-        <button class="button blue" onclick="postForm('form_Abordagem', '<?php echo CAMINHO_VENDAS."planoAcao/include/acao/planoAcaoComplemento.php?id=$idPlanoAcao"?>')" > Enviar</button>
+        <button class="button blue" onclick="postForm('form_Abordagem2', '<?php echo CAMINHO_VENDAS."planoAcao/include/acao/planoAcaoComplemento.php?id=$idPlanoAcao"?>')" > Enviar</button>
       </p>
     </div>
   </form>
