425fb7f5790e032ad2db182950ccfad9f37ec6a5
DESKTOP-OV7AO87\jr <israelspjr@gmail.com>
2021-01-27 18:34:00 -0300

    Update Relatorio.class.php

--- cursos/config/class/Relatorio.class.php
+++ cursos/config/class/Relatorio.class.php
@@ -2264,6 +2264,7 @@ $valor = $RelatorioDesempenho->selectRelatorioDesempenho(" WHERE acompanhamentoC
     //CARREGA DADOS DE TABELAS RELACIONADAS DE FORMA DINAMICA (de acordo com a parametrização do sistema)
     $PsaProfessor = new PsaProfessor();
     $rsPsaProfessor = $PsaProfessor -> selectPsaProfessor(" WHERE excluido = 0 AND inativo = 0 AND tipo = ".$tipo);
+	Uteis::pr($rsPsaProfessor);
 
 	$totalNotas = 0;
 	$totalProfessor = 0;
