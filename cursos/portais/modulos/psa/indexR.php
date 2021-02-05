83ea5dd0536ba17a312fbf7fa021d4adc7816f40
DESKTOP-OV7AO87\jr <israelspjr@gmail.com>
2021-02-05 15:36:03 -0300

    Update indexR.php

--- cursos/portais/modulos/psa/indexR.php
+++ cursos/portais/modulos/psa/indexR.php
@@ -17,14 +17,17 @@ $arrItens_padrao[] = array(0 => "aluno", 1 => "Aluno");
 $arrItens_padrao[] = array(0 => "dataReferencia", 1 => "Data da pesquisa");
 $arrItens_padrao[] = array(0 => "nomeProfessor", 1 => "Nome Professor");
 
-$rsPsaProfessor = $PsaProfessor->selectPsaProfessor(" WHERE excluido = 0 AND inativo = 0 AND tipo = 4");
-foreach($rsPsaProfessor as $valor) $arrItens_padrao[] = array(0 => $valor['titulo'], 1 => $valor['titulo']);
-$arrItens_padrao[] = array(0 => "gestaoCurso", 1 => "GESTÃO DO CURSO");
+//$rsPsaProfessor = $PsaProfessor->selectPsaProfessor(" WHERE excluido = 0 AND inativo = 0 AND tipo = 4");
+//foreach($rsPsaProfessor as $valor) $arrItens_padrao[] = array(0 => $valor['titulo'], 1 => $valor['titulo']);
+$rsPsaRegular = $PsaRegular->selectPsaRegular(" WHERE (excluido = 0 AND inativo = 0 AND tipo = 1) OR (idPsa = 7) ");
+foreach($rsPsaRegular as $valor) $arrItens_padrao[] = array(0 => $valor['titulo'], 1 => $valor['titulo']);
+
+/*$arrItens_padrao[] = array(0 => "gestaoCurso", 1 => "GESTÃO DO CURSO");
 $arrItens_padrao[] = array(0 => "qualidadeAula", 1 => "QUALIDADE DA AULA");
 $arrItens_padrao[] = array(0 => "resultadoCurso", 1 => "RESULTADO DO CURSO");
 $arrItens_padrao[] = array(0 => "compromisso", 1 => "COMPROMISSO COM O APRENDIZADO");
 $arrItens_padrao[] = array(0 => "nps", 1 => "NPS - Net Promoter Score");
-
+*/
 $dataAtual = date("Y-m-d");
 ?>
 
