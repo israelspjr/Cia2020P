de3e5cdcd29fa26d4700a885d0e3d3ad219bc877
DESKTOP-OV7AO87\jr <israelspjr@gmail.com>
2021-02-15 11:26:28 -0300

    Update index.php

--- cursos/portais/modulos/planoAcao/index.php
+++ cursos/portais/modulos/planoAcao/index.php
@@ -10,7 +10,7 @@ $idPlanoAcao = $_REQUEST['idPlanoAcao'];
   <div id="fechar_nivel" class="fechar" onclick="fecharNivel();" title="Fechar"></div>
 	<br />--><?php
     $PlanoAcao->setIdPlanoAcao($idPlanoAcao); 
-    echo $PlanoAcao->ImprimePlanoAcao("3");    
+    echo $PlanoAcao->ImprimePlanoAcao("2");    
     ?>
 </div>
 <button class="button gray" onclick="zerarCentro();carregarModulo('modulos/ff/index.php', '#centro')">Fechar</button>
