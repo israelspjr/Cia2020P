90321b3854ef1da4d000498e67b89e10a928c4da
DESKTOP-OV7AO87\jr <israelspjr@gmail.com>
2021-02-19 12:46:19 -0300

    Update css.php

--- cursos/config/include/css.php
+++ cursos/config/include/css.php
@@ -1,9 +1,9 @@
+<!-- JQuery Ui -->
+<link href="<?php echo CAMINHO_CFG?>css/jquery-ui.min.css" rel="stylesheet" type="text/css" />
+
 <link rel="stylesheet" href="<?php echo CAMINHO_CFG?>css/zerar.css" type="text/css" />
 <link rel="stylesheet" href="<?php echo CAMINHO_CFG?>css/estilo.css" type="text/css" />
 <link rel="stylesheet" href="<?php echo CAMINHO_CFG?>css/menu.css" type="text/css" />
-
-<!-- JQuery Ui -->
-<link href="<?php echo CAMINHO_CFG?>css/jquery-ui.min.css" rel="stylesheet" type="text/css" />
 
 <!-- data Tables -->
 <link rel="stylesheet" href="<?php echo CAMINHO_CFG?>css/table_jui.css" />
@@ -13,5 +13,5 @@
 <link href="<?php echo CAMINHO_CFG?>css/button.css" type="text/css" media="screen" rel="stylesheet" />
 
  <!-- Portais --> 
-<link href="<?php echo CAMINHO_CFG?>css/datepicker3.css" rel="stylesheet">
+<!--<link href="<?php echo CAMINHO_CFG?>css/datepicker3.css" rel="stylesheet">-->
 <link href="<?php echo CAMINHO_CFG?>css/styles-admin.css" rel="stylesheet">
