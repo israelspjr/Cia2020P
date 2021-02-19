0481f1bb2b4c4428cf5b28eb9992eab66331f314
DESKTOP-OV7AO87\jr <israelspjr@gmail.com>
2021-02-19 14:49:59 -0300

    Update jquery.validate.js

--- cursos/config/js/jquery.validate.js
+++ cursos/config/js/jquery.validate.js
@@ -64,8 +64,24 @@ function ativarForm(){
 			$('.validate p span').hide();
 										
 			/* mascara data */
-			$('.data')
-			.mask('99/99/9999')
+			$('.data').datepicker({
+				closeText: 'Fechar',
+				prevText: '<Anterior',
+				nextText: 'Próximo>',
+				currentText: 'Hoje',
+				monthNames: ['Janeiro','Fevereiro','Março','Abril','Maio','Junho','Julho','Agosto','Setembro','Outubro','Novembro','Dezembro'],
+				monthNamesShort: ['Jan','Fev','Mar','Abr','Mai','Jun','Jul','Ago','Set','Out','Nov','Dez'],
+				dayNames: ['Domingo','Segunda-feira','Terça-feira','Quarta-feira','Quinta-feira','Sexta-feira','Sabado'],
+				dayNamesShort: ['Dom','Seg','Ter','Qua','Qui','Sex','Sab'],
+				dayNamesMin: ['Dom','Seg','Ter','Qua','Qui','Sex','Sab'],
+				weekHeader: 'Sm',
+				dateFormat: 'dd/mm/yy',
+				firstDay: 0,
+				isRTL: false,
+				showMonthAfterYear: false,
+				yearSuffix: ''
+			});
+		//	.mask('99/99/9999')
 			/*.datepicker({					
 				dateFormat: 'dd/mm/yy',
 				dayNames: ['Domingo','Segunda','Terça','Quarta','Quinta','Sexta','Sábado','Domingo'],
