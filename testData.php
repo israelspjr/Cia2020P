7aade6bb4eaea3df72cdefee3245abbfe24f16ad
DESKTOP-OV7AO87\jr <israelspjr@gmail.com>
2021-02-19 12:37:02 -0300

    Update testData.php

--- testData.php
+++ testData.php
@@ -8,7 +8,8 @@
  <!-- <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">-->
  <script src="https://www.companhiadeidiomas.net.br/cursos/config/js/jquery.js"></script>
  <!-- <script src="https://code.jquery.com/jquery-1.12.4.js"></script>-->
-  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+ <script src="https://www.companhiadeidiomas.net.br/cursos/config/js/jquery-ui.js"></script>
+<!--  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>-->
   <script>
   $( function() {
     $( ".data" ).datepicker();
