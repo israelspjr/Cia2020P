61dd68b888b2fb11d61bb1b87207782100f62613
DESKTOP-OV7AO87\jr <israelspjr@gmail.com>
2021-02-19 12:34:57 -0300

    Update testData.php

--- testData.php
+++ testData.php
@@ -1,10 +1,11 @@
 ﻿<!doctype html>
-<html lang="en">
+<html lang="br">
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>jQuery UI Datepicker - Default functionality</title>
-  <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+   <link rel="stylesheet" href="https://www.companhiadeidiomas.net.br/cursos/config/css/jquery-ui.min.css">
+ <!-- <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">-->
  <script src="https://www.companhiadeidiomas.net.br/cursos/config/js/jquery.js"></script>
  <!-- <script src="https://code.jquery.com/jquery-1.12.4.js"></script>-->
   <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
