772cd72b0fc4775333398f910cc233bb67b8f602
DESKTOP-OV7AO87\jr <israelspjr@gmail.com>
2021-02-22 15:13:37 -0300

    Update psa.php

--- cursos/admin/modulos/relatorios/novaPsa/include/resourceHTML/psa.php
+++ cursos/admin/modulos/relatorios/novaPsa/include/resourceHTML/psa.php
@@ -83,7 +83,7 @@ foreach($val as $conceito => $respostas) {
 <tbody>  ';
 $x = 0;
 foreach($respostas as $conceito2 => $respostas2) {
-Uteis::pr($respostas2);
+Uteis::pr($conceitos."-".$respostas2);
 $gerentes .= '<tr>
 <td align="center">'.$conceito2.'</td><td align="center">'.$respostas2.'</td><td align="center">'.round((($respostas2*100)/$respostas['total']),2)."%".'</td>
 </tr>'; 
