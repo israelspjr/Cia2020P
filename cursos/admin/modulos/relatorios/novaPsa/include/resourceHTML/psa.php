5840d4d39494e8c57987f771bfba866c7e2719f0
DESKTOP-OV7AO87\jr <israelspjr@gmail.com>
2021-02-22 16:07:00 -0300

    Update psa.php

--- cursos/admin/modulos/relatorios/novaPsa/include/resourceHTML/psa.php
+++ cursos/admin/modulos/relatorios/novaPsa/include/resourceHTML/psa.php
@@ -137,7 +137,7 @@ if ($val['total'] > 0) {
 ?>
 
 
-   <tr><td>Média</td><td><?php echo round($mediaTotal,2);?></td><td><?php echo $notasGerais;?></td></tr>
+   <tr><td>Média - (<?php echo $notasGerais;?>)</td><td><?php echo round($mediaTotal,2);?></td><td></td></tr>
 </tbody>
 </table>
 </td>
