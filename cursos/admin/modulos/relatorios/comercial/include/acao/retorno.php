df7220baacb5e0e5ea43d1ad22c91aac2ebb35d1
DESKTOP-OV7AO87\jr <israelspjr@gmail.com>
2021-02-22 11:45:47 -0300

    Update retorno.php

--- cursos/admin/modulos/relatorios/comercial/include/acao/retorno.php
+++ cursos/admin/modulos/relatorios/comercial/include/acao/retorno.php
@@ -9,6 +9,27 @@ $arrayRetorno = array();
 //FILTROS
 $where = "";
 
+//MONTA CAMPO
+$campos = $_POST['sel_lista_padrao'];
+$camposNome = $_POST['sel_lista_padraoNome'];
+/*
+$status = $_POST['status'];
+
+if( $status != '' ) {
+	if ($status == 2) {
+		$where .= " AND PF.excluido = 1";
+	} else {
+		$where .= " AND PF.excluido = 0 AND PF.inativo =".$status;
+	}
+}
+/*
+
+$grupo1 = implode(",", $_POST['grupo1']);
+
+
+$tipoCliente_idTipoCliente = implode(",",$_POST['TipoCliente_idTipoCliente']);
+if( $tipoCliente_idTipoCliente ) $where .= " AND PF.tipoCliente_idTipoCliente IN(".$tipoCliente_idTipoCliente.")";
+*/
 $nome = $_POST['nome'];
 if( $nome != '' ):
    $where .= " AND CPF.nome like '%".$nome."%'";
@@ -16,6 +37,8 @@ endif;
 
 
 $IdClientePj = $_POST['clientePj_idClientePj'];
+$area = $_POST['area'];
+$where .= " AND CPF.area = ".$area;
 
 $criterio = $_POST['dataRS'];
 //if ($criterio != '') {
@@ -39,17 +62,51 @@ if ($mes_fim < 10) {
 }
 
 //if( $mes_ini && $ano_ini && $mes_fim && $ano_fim ){
-$dataInicio = $ano_ini."-".$mes_ini."-01";
-$dataFim = $ano_fim."-".$mes_fim."-01";
+$dataInicio = $anoIni."-".$mes_ini."-01";
+$dataFim = $anoFim."-".$mes_fim."-01";
 
 $dataReferenciaFinal = date("Y-m-d", strtotime("-1 days", strtotime("+1 months", strtotime($dataFim))));
 
 $where .= " AND (".$valorCriterio." between '".$dataInicio ."' AND '".$dataReferenciaFinal."' )";
 
-$where .= " ORDER BY dataRetorno ";
+
+//	}
+}
+//echo $where;
+//if($IdClientePj != "-"){
+//if($IdClientePj!= "") $where .= " AND PF.clientePj_idClientePj = ".$IdClientePj; 
+//}
+
+$idIdioma = $_REQUEST['idIdioma'];
+if($idIdioma != "-"){
+	if($idIdioma != "") {
+		$where .= " AND PR.idioma_idIdioma = ".$idIdioma;
+	}
 }
 
-$arrayRetorno['excel'] = $IntegranteGrupo->selectIntegranteGrupoTr_historicoInd($where,0, $IdClientePj,1);
+$IdNivelEstudo = $_REQUEST['IdNivelEstudo'];
+if($IdNivelEstudo != "-"){
+	if($IdNivelEstudo != "") {
+		$where .= " AND PAG.nivelEstudo_IdNivelEstudo = ".$IdNivelEstudo;
+	}
+}
+
+$where .= " ORDER BY dataRetorno ";
+/*
+$grupo_idGrupo = $_POST['grupo_idGrupo'];
+if($grupo_idGrupo != "-"){
+if($grupo_idGrupo!= "") $idGrupo = $grupo_idGrupo; 
+}
+
+
+$pendentes = $_POST['pendentes'];
+if ($pendentes == 'on') {
+	$pendentes = 1;
+}
+*/
+//echo $where;
+
+$arrayRetorno['excel'] = $IntegranteGrupo->selectIntegranteGrupoTr_historicoInd($where,0, $IdClientePj,1,$dataFim, 1, $campos, $camposNome);
 
 echo json_encode($arrayRetorno);
 
